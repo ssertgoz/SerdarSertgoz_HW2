5d8a036f38556462023123fe25d8bf1a285b2c3f
ssertgoz <44555856+ssertgoz@users.noreply.github.com>
2023-05-19 01:05:19 +0300

    Home Screen is running with MVVM

--- NewsApp/NewsApp/Controllers/HomeViewController.swift
+++ NewsApp/NewsApp/Controllers/HomeViewController.swift
@@ -6,27 +6,12 @@
 //
 
 import UIKit
-import NewsAPI
 
-extension HomeViewController {
-    fileprivate enum Constants {
-        static let cellLeftPadding: CGFloat = 10
-        static let cellRightPadding: CGFloat = 10
-        static let cellPosterImageRatio: CGFloat = 1/2
-        static let cellTitleHeight: CGFloat = 60
-    }
-
-}
 
 final class HomeViewController: UIViewController, LoadingShowable {
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var sectionsCollectionView: UICollectionView!
-    
-    private let service: TopNewsServiceProtocol = TopNewsService()
-    private var newsList: [News] = []
-    private var sectionList: [SectionAPIURL] = SectionAPIURL.allCases
-    private var selectedSection: SectionAPIURL = SectionAPIURL.home
     
     //for segue
     var imageURL: String?
@@ -37,15 +22,16 @@ final class HomeViewController: UIViewController, LoadingShowable {
     var writerName: String?
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
+    var homeViewModel: HomeViewModelProtocol!{
+        didSet {
+            homeViewModel.delegate = self
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchMovies()
+        homeViewModel.fetchNewsData(0)
         sectionsCollectionView.register(cellType: SectionCell.self)
         collectionView.register(cellType: NewsCell.self)
         configure()
@@ -56,41 +42,13 @@ final class HomeViewController: UIViewController, LoadingShowable {
         self.title = "The New York Times"
     }
     
-     func fetchMovies(section:SectionAPIURL = SectionAPIURL.home) {
-        self.showLoading()
-        
-        service.fetchTopNews(section: section) { [weak self] response in
-            guard let self else { return }
-            self.hideLoading()
-            switch response {
-            case .success(let news):
-                self.newsList = self.filterNewsList(news)
-                self.selectedSection = section
-                self.collectionView.reloadData()
-                self.sectionsCollectionView.reloadData()
-                let indexPath = IndexPath(item: 0, section: 0)
-                self.collectionView.scrollToItem(at: indexPath, at: .top, animated: true)
-            case .failure(let error):
-                print("Serdar: \(error)")
-            }
-        }
-    }
-    
+         
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         collectionView.collectionViewLayout.invalidateLayout()
         LoadingView.shared.handleOrientationChange()
     }
-    func filterNewsList(_ newsList: [News]) -> [News] {
-        return newsList.filter { news in
-            if let url = news.url, url != "null",
-               let abstract = news.abstract, !abstract.isEmpty {
-                return true
-            } else {
-                return false
-            }
-        }
-    }
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if identifier == "Details" {
             
@@ -118,9 +76,9 @@ extension HomeViewController: UICollectionViewDataSource {
    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == self.collectionView {
-            return newsList.count
+            return homeViewModel.numberOfNewsItem
         } else if collectionView == self.sectionsCollectionView {
-            return sectionList.count
+            return homeViewModel.numberOfSectionsItem
         }
         return 0
     }
@@ -129,38 +87,37 @@ extension HomeViewController: UICollectionViewDataSource {
         let cell = UICollectionViewCell()
 
         if collectionView == self.collectionView {
-            let cell = collectionView.dequeCell(cellType: NewsCell.self, indexPath: indexPath)
-            let news = self.newsList[indexPath.row]
-            cell.configure(news: news)
-            cell.layer.cornerRadius = 8
-            cell.layer.masksToBounds = true
-            return cell
-        } else if collectionView == self.sectionsCollectionView {
-            let cell = collectionView.dequeCell(cellType: SectionCell.self, indexPath: indexPath)
-            let section = self.sectionList[indexPath.row]
-            cell.configure(section: section)
-            if(section == selectedSection){
-                cell.title.textColor = .black
-                cell.contentView.backgroundColor = .systemYellow
-            }else{
-                cell.title.textColor = .white
-                cell.contentView.backgroundColor = .lightGray
+            if let news = self.homeViewModel.newsAt(indexPath.row){
+                let cell = collectionView.dequeCell(cellType: NewsCell.self, indexPath: indexPath)
+                cell.configure(news: news)
+                cell.layer.cornerRadius = 8
+                cell.layer.masksToBounds = true
+                return cell
             }
             
-            cell.layer.cornerRadius = 8
-            cell.layer.masksToBounds = true
-            return cell
+        } else if collectionView == self.sectionsCollectionView {
+            
+            if let section = self.homeViewModel.sectionAt(indexPath.row){
+                let cell = collectionView.dequeCell(cellType: SectionCell.self, indexPath: indexPath)
+                cell.configure(section: section)
+                if(section == homeViewModel.selectedSectionItem){
+                    cell.title.textColor = .black
+                    cell.contentView.backgroundColor = .systemYellow
+                }else{
+                    cell.title.textColor = .white
+                    cell.contentView.backgroundColor = .lightGray
+                }
+                
+                cell.layer.cornerRadius = 8
+                cell.layer.masksToBounds = true
+                return cell
+            }
+            
+            
         }
         
         
         return cell
-    }
-    
-    private func calculateHeight() -> CGFloat {
-        let cellWitdh = collectionView.frame.size.width - (Constants.cellLeftPadding + Constants.cellRightPadding)
-        let posterImageHeight = cellWitdh * Constants.cellPosterImageRatio
-        
-        return (Constants.cellTitleHeight + posterImageHeight)
     }
 }
 
@@ -168,31 +125,59 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == self.sectionsCollectionView{
-            return CGSize(width: (self.sectionList[indexPath.row] as SectionAPIURL).rawValue.count*15, height: 35)
+            return CGSize(
+                width: homeViewModel.calculateSectionCellSize(indexPath.row).width,
+                height: homeViewModel.calculateSectionCellSize(indexPath.row).height
+            )
         }
-        return CGSize(width: collectionView.frame.size.width, height: 200)
+        return CGSize(width: collectionView.frame.size.width, height: homeViewModel.heightOfNewsCell)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        .init(top: Constants.cellLeftPadding, left: Constants.cellLeftPadding, bottom: Constants.cellLeftPadding, right: Constants.cellRightPadding)
+        .init(
+            top: homeViewModel.cellPaddings.top,
+            left: homeViewModel.cellPaddings.left,
+            bottom: homeViewModel.cellPaddings.bottom,
+            right: homeViewModel.cellPaddings.right
+        )
         
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView == self.collectionView {
-            let originalString = (newsList[indexPath.row].publishedDate ?? "") as String
+            let news = homeViewModel.newsAt(indexPath.row)
+            let originalString = (news?.publishedDate ?? "" ) as String
             let substring = originalString.prefix(10)
-            self.titleString = newsList[indexPath.row].title
-            self.writerName = newsList[indexPath.row].byline
             self.date = String(substring)
-            self.desc = newsList[indexPath.row].abstract
-            self.imageURL = newsList[indexPath.row].multimedia?[1].url ?? ""
-            self.readMoreURL = newsList[indexPath.row].url
+            self.titleString = news?.title
+            self.writerName = news?.byline
+            
+            self.desc = news?.abstract
+            self.imageURL = news?.multimedia?[1].url ?? ""
+            self.readMoreURL = news?.url
             performSegue(withIdentifier: "Details", sender: indexPath)
             
         } else if collectionView == self.sectionsCollectionView {
-            self.fetchMovies(section: self.sectionList[indexPath.row])
+            homeViewModel.fetchNewsData(indexPath.row)
         }
         
     }
 }
 
+extension HomeViewController: HomeViewModelDelegate{
+    func showLoadingView() {
+        showLoading()
+    }
+    
+    func hideLoadingView() {
+        hideLoading()
+    }
+    
+    func reloadData() {
+        collectionView.reloadData()
+        sectionsCollectionView.reloadData()
+        let indexPath = IndexPath(item: 0, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .top, animated: true)
+    }
+    
+    
+}
--- NewsApp/NewsApp/Scenes/ViewModels/HomeViewModel.swift
+++ NewsApp/NewsApp/Scenes/ViewModels/HomeViewModel.swift
@@ -6,18 +6,125 @@
 //
 
 import Foundation
+import NewsAPI
+
+
+extension HomeViewModel {
+    fileprivate enum Constants {
+        static let sectionCellHeight: Double = 35
+        static let sectionCellWidthFactor: Double = 15
+        static let newsCellHeight: Double = 200
+        static let cellLeftPadding: Double = 10
+        static let cellRightPadding: Double = 10
+        static let cellTopPadding: Double = 10
+        static let cellBottomPadding: Double = 10
+    }
+}
 
 
 protocol HomeViewModelProtocol{
-    
+    var delegate: HomeViewModelDelegate? { get set }
+    func fetchNewsData(_ index: Int)
+    var numberOfNewsItem: Int { get }
+    func newsAt(_ index: Int) -> News?
+    var numberOfSectionsItem: Int { get }
+    func sectionAt(_ index: Int) -> SectionAPIURL?
+    var selectedSectionItem: SectionAPIURL? { get }
+    func calculateSectionCellSize(_ index: Int) -> (width: Double, height: Double)
+    var heightOfNewsCell: Double { get }
+    var cellPaddings: (top: Double, left: Double, bottom: Double, right: Double) { get }
 }
 
 
 protocol HomeViewModelDelegate: AnyObject {
-    
+    func showLoadingView()
+    func hideLoadingView()
+    func reloadData()  //MARK: do it for both section and normal collection view
 }
 
 
 final class HomeViewModel{
+    let service: TopNewsServiceProtocol
+    private var newsList: [News] = []
+    private var sectionList: [SectionAPIURL] = SectionAPIURL.allCases
+    private var selectedSection: SectionAPIURL = SectionAPIURL.home
+    
+    weak var delegate: HomeViewModelDelegate?
+    
+    init(service: TopNewsServiceProtocol) {
+        self.service = service
+    }
+    
+    
+    fileprivate func fetchNews(_ index: Int) {
+        self.delegate?.showLoadingView()
+       
+       service.fetchTopNews(section: sectionAt(index) ?? SectionAPIURL.home ) { [weak self] response in
+           guard let self else { return }
+           self.delegate?.hideLoadingView()
+           switch response {
+           case .success(let news):
+               self.newsList = self.filterNewsList(news)
+               self.selectedSection = self.sectionAt(index) ?? SectionAPIURL.home
+               self.delegate?.reloadData()
+           case .failure(let error):
+               print("Serdar: \(error)")
+           }
+       }
+   }
+    
+    
+    fileprivate func filterNewsList(_ newsList: [News]) -> [News] {
+        return newsList.filter { news in
+            if let url = news.url, url != "null",
+               let abstract = news.abstract, !abstract.isEmpty {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+}
+
+extension HomeViewModel: HomeViewModelProtocol{
+    var cellPaddings: (top: Double, left: Double, bottom: Double, right: Double) {
+        return (top: Constants.cellTopPadding, left: Constants.cellLeftPadding, bottom: Constants.cellBottomPadding, right: Constants.cellRightPadding)
+    }
+    
+    var heightOfNewsCell: Double {
+        Constants.newsCellHeight
+    }
+    
+    func calculateSectionCellSize(_ index: Int) -> (width: Double, height: Double) {
+        let height = Constants.sectionCellHeight
+        let width = Double((sectionAt(index))?.rawValue.count ?? 0)*Constants.sectionCellWidthFactor
+        return (width: width,height: height)
+    }
+    
+    var numberOfSectionsItem: Int {
+        sectionList.count
+    }
+    
+    func sectionAt(_ index: Int) -> SectionAPIURL? {
+        sectionList[index]
+    }
+    
+    func newsAt(_ index: Int) -> News? {
+        newsList[index]
+    }
+    
+    var selectedSectionItem: SectionAPIURL? {
+        selectedSection
+    }
+    
+    var numberOfNewsItem: Int {
+        newsList.count
+    }
+    
+    func fetchNewsData(_ index: Int = 0) {
+        fetchNews(index)
+    }
+    
     
 }
